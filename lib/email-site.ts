afd9b2d0c6357de930937737e4f27b25ffade531
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-12-09 17:34:27 +0100

    update query in DatabaseSync to use dynamic key columns for site filtering

---
+++ lib/email-site.ts
@@ -0,0 +1,48 @@
+import { create } from "zustand";
+
+export type FieldPair = {
+  id: string;
+  site: string;
+  email_address: string;
+};
+
+type FormStore = {
+  fields: FieldPair[];
+  addFieldPair: () => void;
+  removeFieldPair: (id: string) => void;
+  updateFieldPair: (id: string, updates: Partial<FieldPair>) => void;
+  setFields: (fields: FieldPair[]) => void;
+  resetForm: () => void;
+};
+
+export const useFormStore = create<FormStore>((set) => ({
+  fields: [{ id: "1", site: "", email_address: "" }],
+
+  addFieldPair: () =>
+    set((state) => ({
+      fields: [
+        ...state.fields,
+        {
+          id: Date.now().toString(),
+          site: "",
+          email_address: "",
+        },
+      ],
+    })),
+
+  removeFieldPair: (id: string) =>
+    set((state) => ({
+      fields: state.fields.filter((field) => field.id !== id),
+    })),
+
+  updateFieldPair: (id: string, updates: Partial<FieldPair>) =>
+    set((state) => ({
+      fields: state.fields.map((field) =>
+        field.id === id ? { ...field, ...updates } : field
+      ),
+    })),
+
+  setFields: (fields: FieldPair[]) => set({ fields }),
+
+  resetForm: () => set({ fields: [{ id: "1", site: "", email_address: "" }] }),
+}));

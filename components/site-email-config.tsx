3171d7c5fe2146bdf1845c0382e6eb0c056721ae
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-12-11 10:00:28 +0100

    update query in DatabaseSync to use dynamic key columns for site filtering

--- components/site-email-config.tsx
+++ components/site-email-config.tsx
@@ -41,7 +41,7 @@ export default function Home() {
     }
 
     const payload = fields.map((field) => ({
-      site: field.site,
+      site_url: field.site,
       email_address: field.email_address,
     }));
 
